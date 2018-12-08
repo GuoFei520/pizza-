164197ced5807d43df32344ed097f3d66d5a4a87
guofei <18300852282@163.com>
2018-12-08 17:43:21 +0800

    更新项目

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,28 +1,34 @@
 <template>
-    <h1>Admin</h1>
+    <div class="row">
+        <div class="col-sm-12 col-md-8">
+            <!-- 添加pizza -->
+            <app-new-pizza></app-new-pizza>
+        </div>
+        <div class="col-sm-12 col-md-4">
+            <!-- 品种展示 -->
+            <h3 class="text-muted my-5">菜单</h3>
+            <table class="table">
+                <thead class="table table-default">
+                    <tr>
+                        <th>品种</th>
+                        <th>删除</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <td>榴莲</td>
+                    <td>
+                        <button class="btn-outline-danger btn-sm">&times;</button>
+                    </td>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </template>
 <script>
+import NewPizza from "./NewPizza.vue";
     export default {
-        data(){
-            return {
-                name:'Henry'
-            }
-        },
-        // beforeRouteEnter (to, from, next) {
-            // ...
-            // alert("Hello" + this.name);
-            // next();
-            // next(vm=>{
-                // alert("Hello" + vm.name);
-            // })
-        // }
-        // beforeRouteLeave (to, from, next) {
-        //     // ...
-        //     if(confirm("确定离开吗？")=== true){
-        //         next();
-        //     }else{
-        //         next(false);
-        //     }
-        // }
+        components:{
+            'app-new-pizza':NewPizza
+        }
     }
 </script>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        <div class="col-sm-12">
+    <div class="row">
+        <!-- 菜单 -->
+        <div class="col-sm-12 col-md-8">
             <table class="table">
                 <thead class="thead-default">
                     <tr>
@@ -16,10 +17,42 @@
                     <tr v-for="option in item.options" :key="option.size">
                         <td>{{option.size}}</td>
                         <td>{{option.price}}</td>
-                        <td><button class="btn btn-sm btn-outline-success">+</button></td>
+                        <td>
+                            <button @click="addToBasket(item,option)" class="btn btn-sm btn-outline-success">+</button>
+                        </td>
                     </tr>
                 </tbody>
             </table>
+        </div>
+        <!-- 购物车 -->
+        <div class="col-sm-12 col-md-4">
+            <div v-if="baskets.length > 0">
+                <table class="table">
+                    <thead class="thead-default">
+                        <tr>
+                            <th>数量</th>
+                            <th>品种</th>
+                            <th>价格</th>
+                        </tr>
+                    </thead>
+                    <tbody v-for="item in baskets" :key="item.name">
+                        <tr>
+                            <td>
+                                <button class="btn btn-sm" @click="decreaseQuantity(item)">-</button>
+                                <span>&nbsp;{{item.quantity}}&nbsp;</span>
+                                <button class="btn btn-sm" @click="increaseQuantity(item)">+</button>
+                            </td>
+                            <td>{{item.name}}{{item.size}}</td>
+                            <td>{{item.price * item.quantity}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p>总价：{{total + "RMB"}}</p>
+                <button class="btn btn-success btn-block">提交</button>
+            </div>
+            <div v-else>
+                {{basketText}}
+            </div>
         </div>
     </div>
 </template>
@@ -27,6 +60,8 @@
     export default{
         data(){
             return{
+                baskets:[],
+                basketText:"购物车没有任何商品",
                 getMenuItem:{
                     1: {
                     'name': '榴莲pizza',
@@ -62,6 +97,51 @@
                     }]
                     }
                 }
+            }
+        },
+        computed:{
+            total(){
+                let totalCost = 0;
+                for (let index in this.baskets) {
+                    let individuaItem = this.baskets[index]
+                    totalCost += individuaItem.quantity*individuaItem.price
+                }
+                return totalCost
+            }
+        },
+        methods:{
+            addToBasket(item,option){
+                let basket = {
+                    name:item.name,
+                    size:option.size,
+                    price:option.price,
+                    quantity:1
+                }
+                if(this.baskets.length > 0){
+                    // 过滤
+                    let result = this.baskets.filter((basket)=>{
+                        return basket.name === item.name&&basket.price === option.price
+                    })
+                    if(result != null && result.length > 0){
+                        result[0].quantity++
+                    }else {
+                        this.baskets.push(basket)
+                    }
+                }else {
+                    this.baskets.push(basket)
+                }
+            },
+            decreaseQuantity(item){
+                item.quantity--;
+                if(item.quantity<=0) {
+                    this.removeFromBasket(item)
+                }
+            },
+            increaseQuantity(item){
+                item.quantity++;
+            },
+            removeFromBasket(item) {
+                this.baskets.splice(this.baskets.indexOf(item),1);
             }
         }
     }
